c2a3eefc36c130081fe114c10b45157f030d4c8d
yubin <yubin@itdadev.com>
2022-03-03 03:18:08 +0900

    created routes, register api created. WIP : register logic

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const connectDB = require("./config/db");
+
 const app = express();
-
-app.get("/", (req,res) => res.send("API running"));
-
 const PORT = process.env.PORT || 5000;
+
+// Connect to database
+connectDB();
+
+// Initialize middleware
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => res.send("API running"));
+
+// Define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
