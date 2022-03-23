07488e1f63a47b53d527aeca1512359fe3855e80
yubin <yubin@itdadev.com>
2022-03-23 18:16:17 +0900

    profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,86 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
 
-// @route   GET api/profile
-// @desc    Test route
-// @access  public
-router.get('/', (req,res)=> res.send("Profile route"))
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+// @route   GET api/profile/me
+// @desc    Get user id in the token
+// @access  private
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "user",
+      ["name", "avatar"]
+    );
+
+    if (!profile)
+      return res.status(400).json({ msg: "Profile does not exist" });
+
+    res.json(profile);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).send("Server error");
+  }
+});
+
+// @route   POST api/profile
+// @desc    Create or update user profile
+// @access  private
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("status", "Status is required").not().isEmpty(),
+      check("skills", "Skill is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin,
+    } = req.body;
+
+    // Build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+
+    if (skills)
+      profileFields.skills = skills.split(",").map((skill) => skill.trim());
+
+    if (youtube) profileFields.youtube = youtube;
+    if (facebook) profileFields.facebook = facebook;
+    if (twitter) profileFields.twitter = twitter;
+    if (instagram) profileFields.instagram = instagram;
+    if (linkedin) profileFields.linkedin = linkedin;
+
+    console.log(skills);
+    res.send("hello");
+  }
+);
 
 module.exports = router;
